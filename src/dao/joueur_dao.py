d2e0262360fa6c5b758841a5fe931ec535b1c2bb
Victor Gautier <victor.gautier@eleve.ensai.fr>
2025-10-01 16:30:14 +0200

    Création classes utilisateur

---
+++ src/dao/joueur_dao.py
@@ -0,0 +1,245 @@
+import logging
+
+from utils.singleton import Singleton
+from utils.log_decorator import log
+
+from dao.db_connection import DBConnection
+
+from business_object.utilisateur import Utilisateur
+
+
+class UtilisateurDao(metaclass=Singleton):
+    """Classe contenant les méthodes pour accéder aux Utilisateurs de la base de données"""
+
+    @log
+    def creer(self, utilisateur) -> bool:
+        """Creation d'un utilisateur dans la base de données
+
+        Parameters
+        ----------
+        utilisateur : Utilisateur
+
+        Returns
+        -------
+        created : bool
+            True si la création est un succès
+            False sinon
+        """
+
+        res = None
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "INSERT INTO utilisateur(nom_user, email_user, mdp) VALUES        "
+                        "(%(nom_user)s, %(email_user)s, %(mail)s, %(mdp)s)             "
+                        "  RETURNING id_user;                                                ",
+                        {
+                            "nom_user": utilisateur.nom_user,
+                            "email_user": utilisateur.email_user,
+                            "mdp": utilisateur.mdp,
+                        },
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+
+        created = False
+        if res:
+            utilisateur.id_user = res["id_user"]
+            created = True
+
+        return created
+
+    @log
+    def trouver_par_id(self, id_user) -> Utilisateur:
+        """trouver un utilisateur grace à son id
+
+        Parameters
+        ----------
+        id_user : int
+            numéro id de l'utilisateur que l'on souhaite trouver
+
+        Returns
+        -------
+        utilisateur : Utilisateur
+            renvoie l'utilisateur que l'on cherche par id
+        """
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "SELECT *                           "
+                        "  FROM utilisateur                      "
+                        " WHERE id_user = %(id_user)s;  ",
+                        {"id_user": id_user},
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+            raise
+
+        utilisateur = None
+        if res:
+            utilisateur = Utilisateur(
+                nom_user=res["nom_user"],
+                email_user=res["email_user"],
+                mdp=res["mdp"],
+                id_user=res["id_user"],
+            )
+
+        return utilisateur
+
+    @log
+    def lister_tous(self) -> list[Utilisateur]:
+        """lister tous les utilisateurs
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        liste_utilisateurs : list[Utilisateur]
+            renvoie la liste de tous les utilisateurs dans la base de données
+        """
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "SELECT *                              "
+                        "  FROM utilisateur;                        "
+                    )
+                    res = cursor.fetchall()
+        except Exception as e:
+            logging.info(e)
+            raise
+
+        liste_utilisateurs = []
+
+        if res:
+            for row in res:
+                utilisateur = Utilisateur(
+                    id_user=row["id_user"],
+                    nom_user=row["nom_user"],
+                    mdp=row["mdp"],
+                    email_user=row["email_user"]
+                )
+
+                liste_utilisateurs.append(utilisateur)
+
+        return liste_utilisateurs
+
+    @log
+    def modifier(self, utilisateur) -> bool:
+        """Modification d'un utilisateur dans la base de données
+
+        Parameters
+        ----------
+        utilisateur : Utilisateur
+
+        Returns
+        -------
+        created : bool
+            True si la modification est un succès
+            False sinon
+        """
+
+        res = None
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "UPDATE utilisateur                                      "
+                        "   SET nom_user      = %(nom_user)s,                   "
+                        "       email_user         = %(email_user)s,                      "
+                        "       mdp = %(mdp)s               "
+                        " WHERE id_user = %(id_user)s;                  ",
+                        {
+                            "nom_user": utilisateur.nom_user,
+                            "email_user": utilisateur.email_user,
+                            "mdp": utilisateur.mdp,
+                            "id_user": utilisateur.id_user,
+                        },
+                    )
+                    res = cursor.rowcount
+        except Exception as e:
+            logging.info(e)
+
+        return res == 1
+
+    @log
+    def supprimer(self, utilisateur) -> bool:
+        """Suppression d'un utilisateur dans la base de données
+
+        Parameters
+        ----------
+        utilisateur : Utilisateur
+            utilisateur à supprimer de la base de données
+
+        Returns
+        -------
+            True si le utilisateur a bien été supprimé
+        """
+
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    # Supprimer le compte d'un utilisateur
+                    cursor.execute(
+                        "DELETE FROM utilisateur                  "
+                        " WHERE id_user=%(id_user)s      ",
+                        {"id_user": utilisateur.id_user},
+                    )
+                    res = cursor.rowcount
+        except Exception as e:
+            logging.info(e)
+            raise
+
+        return res > 0
+
+    @log
+    def se_connecter(self, nom_user, mdp) -> Utilisateur:
+        """se connecter grâce à son nom_user et son mot de passe
+
+        Parameters
+        ----------
+        nom_user : str
+            nom_user du utilisateur que l'on souhaite trouver
+        mdp : str
+            mot de passe du utilisateur
+
+        Returns
+        -------
+        utilisateur : Utilisateur
+            renvoie le utilisateur que l'on cherche
+        """
+        res = None
+        try:
+            with DBConnection().connection as connection:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        "SELECT *                           "
+                        "  FROM utilisateur                      "
+                        " WHERE nom_user = %(nom_user)s         "
+                        "   AND mdp = %(mdp)s;              ",
+                        {"nom_user": nom_user, "mdp": mdp},
+                    )
+                    res = cursor.fetchone()
+        except Exception as e:
+            logging.info(e)
+
+        utilisateur = None
+
+        if res:
+            utilisateur = Utilisateur(
+                nom_user=res["nom_user"],
+                mdp=res["mdp"],
+                email_user=res["email_user"]
+                id_user=res["id_user"],
+            )
+
+        return utilisateur
---
+++ src/service/utilisateur_service.py
@@ -0,0 +1,81 @@
+from tabulate import tabulate
+
+from utils.log_decorator import log
+from utils.securite import hash_password
+
+from business_object.utilisateur import Utilisateur
+
+
+class UtilisateurService:
+    """Classe contenant les méthodes de service des Utilisateurs"""
+
+    @log
+    def creer(self, id_user, nom_user, mail_user, mdp) -> Utilisateur:
+        """Création d'un utilisateur"""
+
+        nouvel_utilisateur = Utilisateur(
+            id_user = id_user,
+            nom_user = nom_user,
+            mail_user = mail_user,
+            mdp = hash_password(mdp, nom_user)
+        )
+
+        return nouvel_utilisateur  if UtilisateurDao().creer(nouvel_utilisateur) else None
+
+    @log
+    def trouver_par_id(self, id_user) -> Utilisateur:
+        """Trouver un utilisateur à partir de son id"""
+        return UtilisateurDao().trouver_par_id(id_user)
+
+    @log
+    def modifier(self, utilisateur) -> Utilisateur:
+        """Modification d'un utilisateur"""
+
+        utilisateur.mdp = hash_password(utilisateur.mdp, utilisateur.nom_user)
+        return utilisateur if UtilisateurDao().modifier(utilisateur) else None
+
+    @log
+    def supprimer(self, utilisateur) -> bool:
+        """Supprimer le compte d'un utilisateur"""
+        return UtilisateurDao().supprimer(utilisateur)
+
+    @log
+    def afficher_tous(self) -> str:
+        """Afficher tous les utilisateurs
+        Sortie : Une chaine de caractères mise sous forme de tableau
+        """
+        entetes = ["nom", "mail"]
+
+        utilisateurs = UtilisateurDao().lister_tous()
+
+        for j in utilisateurs:
+            if j.nom_user == "admin":
+                utilisateurs.remove(j)
+
+        utilisateurs_as_list = [j.as_list() for j in utilisateurs]
+
+        str_utilisateurs = "-" * 100
+        str_utilisateurs += "\nListe des utilisateurs \n"
+        str_utilisateurs += "-" * 100
+        str_utilisateurs += "\n"
+        str_utilisateurs += tabulate(
+            tabular_data=utilisateurs_as_list,
+            headers=entetes,
+            tablefmt="psql",
+            floatfmt=".2f",
+        )
+        str_utilisateurs += "\n"
+
+        return str_utilisateurs
+
+    @log
+    def se_connecter(self, nom_user, mdp) -> Utilisateur:
+        """Se connecter à partir de nom_user et mdp"""
+        return UtilisateurDao().se_connecter(nom_user, hash_password(mdp, nom_user))
+
+    @log
+    def nom_user_deja_utilise(self, nom_user) -> bool:
+        """Vérifie si le nom_user est déjà utilisé
+        Retourne True si le nom_user existe déjà en BDD"""
+        utilisateurs = UtilisateurDao().lister_tous()
+        return nom_user in [j.nom_user for j in utilisateurs]
