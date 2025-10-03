62adc4c521aea2277084aff9cec347d660bd6e92
MariusAnicet <marius.anicet@gmail.com>
2025-10-03 11:58:03 +0200

    commit

--- src/business_object/statistiques.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict
-
-
-class Statistiques:
-    """Classe pour calculer les statistiques d'un utilisateur suite à ses activités"""
-
-    def calculer_statistiques(self, id_user: int) -> Dict:
-        """Calcule les statistiques globales pour un utilisateur"""
-        pass
