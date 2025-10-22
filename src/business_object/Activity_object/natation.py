3bbd7217cb5401957eca25476eb99510e3c3456d
MariusAnicet <marius.anicet@gmail.com>
2025-10-22 12:00:26 +0200

    classes sport

--- src/business_object/Activity_object/natation.py
+++ src/business_object/Activity_object/natation.py
@@ -18,5 +18,18 @@ class Natation(AbstractActivity):
         super().__init__(id_activite, titre, description, date_activite, duree, distance, id_user)
         self.type_nage = type_nage
 
+    def vitesse(self):
+        """
+        Dans la nage, la vitesse est souvent mesurée en mètres par seconde (m/s).
+        Calcule la vitesse en m/s (mètres par seconde)
+        Returns:
+            float: vitesse en m/s
+        """
+        if self.duree > 0:
+            # distance en km, durée en minutes -> vitesse en m/s
+            # Conversion: km -> m (*1000) et minutes -> secondes (*60)
+            return (self.distance * 1000) / (self.duree * 60)
+        return 0
+
     def __str__(self):
-        return f" {self.type_nage} - {super().__str__()}"
+        return f"{self.type_nage} - {super().__str__()} - Vitesse moyenne: {self.vitesse():.2f} m/s"
--- src/business_object/Activity_object/randonnee.py
+++ src/business_object/Activity_object/randonnee.py
@@ -18,5 +18,16 @@ class Randonnee(AbstractActivity):
         super().__init__(id_activite, titre, description, date_activite, duree, distance, id_user)
         self.type_terrain = type_terrain
 
+    def vitesse(self):
+        """
+        Calcule la vitesse en km/h
+        Return:
+            float: vitesse en km/h
+        """
+        if self.duree > 0:
+            # distance en km, durée en minutes -> vitesse en km/h
+            return (self.distance / self.duree) * 60
+        return 0
+
     def __str__(self):
-        return f" Randonnée - {self.type_terrain} - {super().__str__()}"
+        return f"Randonnée - {self.type_terrain} - {super().__str__()} - Vitesse moyenne : {self.vitesse():.2f} km/h"
