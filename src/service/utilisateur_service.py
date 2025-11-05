c19a7602d01813b3a9bb8f3a0a277076f7008545
TeodoraHub <teodora.moldovan@eleve.ensai.fr>
2025-11-05 11:13:13 +0000

    MAJ utilisateur service

--- src/service/utilisateur_service.py
+++ src/service/utilisateur_service.py
@@ -1,5 +1,4 @@
 from tabulate import tabulate
-
 from business_object.user_object.utilisateur import Utilisateur
 from dao.utilisateur_dao import UtilisateurDAO
 from utils.log_decorator import log
@@ -10,16 +9,21 @@ class UtilisateurService:
     """Classe contenant les méthodes de service des Utilisateurs"""
 
     @log
-    def creer(self, id_user, nom_user, mail_user, mdp) -> Utilisateur:
+    def creer(self, nom_user, mail_user, mdp) -> Utilisateur:
         """Création d'un utilisateur"""
-
+        # Générer automatiquement l'ID
+        utilisateurs = UtilisateurDAO().lister_tous()
+        if utilisateurs:
+            nouvel_id = max([u.id_user for u in utilisateurs]) + 1
+        else:
+            nouvel_id = 1
+        
         nouvel_utilisateur = Utilisateur(
-            id_user=id_user,
+            id_user=nouvel_id,
             nom_user=nom_user,
             mail_user=mail_user,
             mdp=hash_password(mdp, nom_user),
         )
-
         return nouvel_utilisateur if UtilisateurDAO().creer(nouvel_utilisateur) else None
 
     @log
@@ -30,7 +34,6 @@ class UtilisateurService:
     @log
     def modifier(self, utilisateur) -> Utilisateur:
         """Modification d'un utilisateur"""
-
         utilisateur.mdp = hash_password(utilisateur.mdp, utilisateur.nom_user)
         return utilisateur if UtilisateurDAO().modifier(utilisateur) else None
 
@@ -45,15 +48,12 @@ class UtilisateurService:
         Sortie : Une chaine de caractères mise sous forme de tableau
         """
         entetes = ["nom", "mail"]
-
         utilisateurs = UtilisateurDAO().lister_tous()
-
-        for j in utilisateurs:
-            if j.nom_user == "admin":
-                utilisateurs.remove(j)
-
+        
+        # ✅ Filtrer sans modifier la liste pendant l'itération
+        utilisateurs = [u for u in utilisateurs if u.nom_user != "admin"]
+        
         utilisateurs_as_list = [j.as_list() for j in utilisateurs]
-
         str_utilisateurs = "-" * 100
         str_utilisateurs += "\nListe des utilisateurs \n"
         str_utilisateurs += "-" * 100
@@ -65,7 +65,6 @@ class UtilisateurService:
             floatfmt=".2f",
         )
         str_utilisateurs += "\n"
-
         return str_utilisateurs
 
     @log
@@ -79,3 +78,10 @@ class UtilisateurService:
         Retourne True si le nom_user existe déjà en BDD"""
         utilisateurs = UtilisateurDAO().lister_tous()
         return nom_user in [j.nom_user for j in utilisateurs]
+    
+    @log
+    def mail_deja_utilise(self, mail_user) -> bool:
+        """Vérifie si le mail est déjà utilisé
+        Retourne True si le mail existe déjà en BDD"""
+        utilisateurs = UtilisateurDAO().lister_tous()
+        return mail_user in [j.mail_user for j in utilisateurs]
