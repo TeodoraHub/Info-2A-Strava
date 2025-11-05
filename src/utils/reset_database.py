0e2ed0144ee5e67a7a39ae0749c6f674721b3ded
Abelcornetcarlos <abelcornetcarlos@gmail.com>
2025-11-05 10:31:27 +0000

    reset

--- src/utils/reset_database.py
+++ src/utils/reset_database.py
@@ -1,19 +1,31 @@
+import os
 from utils.singleton import Singleton
 from dao.db_connection import DBConnection
 
+
 class ResetDatabase(metaclass=Singleton):
     """
-    Reinitialisation de la base de données
+    Réinitialisation de la base de données
     """
 
     def lancer(self):
         print("Ré-initialisation de la base de données")
 
-        init_db = open("data/init_db.sql", encoding="utf-8")
-        init_db_as_string = init_db.read()
+        # Dossier courant = dossier du script utils/
+        base_dir = os.path.dirname(os.path.abspath(__file__))
 
-        pop_db = open("data/pop_db.sql", encoding="utf-8")
-        pop_db_as_string = pop_db.read()
+        # Aller dans ../data (si data est à la racine du projet)
+        data_dir = os.path.join(base_dir, "..","..", "data")
+
+        init_db_path = os.path.join(data_dir, "init_db.sql")
+        pop_db_path = os.path.join(data_dir, "pop_db.sql")
+
+        # Chargement des fichiers
+        with open(init_db_path, encoding="utf-8") as init_db:
+            init_db_as_string = init_db.read()
+
+        with open(pop_db_path, encoding="utf-8") as pop_db:
+            pop_db_as_string = pop_db.read()
 
         try:
             with DBConnection().connection as connection:
@@ -25,7 +37,6 @@ class ResetDatabase(metaclass=Singleton):
             raise
 
         print("Ré-initialisation de la base de données - Terminée")
-
         return True
 
 
