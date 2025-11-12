9176243a004fd8dae4ebb26a972ff670807d0806
Victor-GAUTIER <victor.gautier@eleve.ensai.fr>
2025-11-12 09:43:42 +0000

    Suppression des modèles

--- src/dao/models/models.py
+++
@@ -1,5 +0,0 @@
-from src.business_object.user_object.utilisateur import Utilisateur
-from src.business_object.like_comment_object.commentaire import Commentaire
-from src.dao.activity_model import Activite
-
-from src.dao.activity_model import Base
